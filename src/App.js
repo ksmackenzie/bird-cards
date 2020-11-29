44c048af8f69601541598b9ee68309781d5f208f
Ken Mackenzie <macseaforth@gmail.com>
2020-11-29 21:21:39 +1000

    added modal alert for search fails, general refactoring, added utilities css file.

--- src/App.js
+++ src/App.js
@@ -3,26 +3,40 @@ import './App.css'
 import Card from './Card'
 import { useState, useEffect } from 'react'
 
+const baseUrl = `https://en.wikipedia.org/w/api.php?origin=*&format=json&action=query&prop=extracts|pageimages&piprop=thumbnail&pithumbsize=500&exintro&redirects=1&titles=`
+
 function App() {
-  const getWikiContent = (topic) => {
-    fetch(
-      `https://en.wikipedia.org/w/api.php?origin=*&format=json&action=query&prop=extracts|pageimages&piprop=original&exintro&redirects=1&titles=${topic}`
-    )
-      .then((res) => res.json())
-      .then((res) => {
-        const card = Object.values(res.query.pages)[0]
-        console.log(res)
-        setnewCard({
-          heading: card.title,
-          description: card.extract,
-          image: card.original.source,
-          id: card.pageid,
-        })
+  function clearModal() {
+    const modal = document.querySelector('.modal-bg')
+    modal?.parentNode && modal.parentNode.removeChild(modal)
+  }
+  const getWikiContent = async (topic) => {
+    try {
+      const response = await fetch(`${baseUrl}${topic}`)
+      const result = await response.json()
+
+      const pageKey = Object.keys(result.query.pages)[0]
+      if (pageKey === '-1') throw new Error(`Oh no! Couldn't find anything on ${topic}`)
+
+      const page = Object.values(result.query.pages)[0]
+      console.log(page)
+      const [heading, description, image, id] = [
+        page.title,
+        page.extract,
+        page.thumbnail?.source || null,
+        page.pageid,
+      ]
+
+      const existingIds = cards.map((c) => c.id)
+      if (!existingIds.includes(page.pageid)) {
+        setnewCard({ heading, description, image, id })
         setquery('')
-      })
-      .catch((e) => {
-        alert(`Oh no! Couldn't find anything on ${topic}`)
-      })
+      }
+    } catch (error) {
+      const app = document.querySelector('.App')
+      const modal = `<div class='modal-bg'><div class='modal'><h3>Failed :(</h3><p>${error.message}</p></div></div>`
+      app.insertAdjacentHTML('afterbegin', modal)
+    }
   }
 
   const [cards, setcards] = useState([])
@@ -35,34 +49,65 @@ function App() {
 
   useEffect(() => {
     newCard && setcards((cards) => [newCard, ...cards])
+    return () => {
+      setnewCard(null)
+    }
   }, [newCard])
+
+  // MODAL
+  useEffect(() => {
+    const clickListener = document.addEventListener('click', (e) => {
+      e.target.className === 'modal-bg' && clearModal()
+    })
+    const clearModalKeyListener = document.addEventListener(
+      'keydown',
+      (e) => e.key === 'Escape' && clearModal(),
+      false
+    )
+
+    return () => {
+      document.removeEventListener('click', clickListener)
+      document.removeEventListener('keydown', clearModalKeyListener)
+    }
+  }, [])
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    addNewCard(query)
+    query && addNewCard(query)
+  }
+
+  const deleteCard = (id) => {
+    console.log(id)
+    setcards(cards.filter((card) => card.id !== id))
   }
 
   return (
-    <div className='App'>
+    <div className='App v-fill-screen grid-burger'>
       <nav>Bird Cards</nav>
       <main>
         <div className='card card-container'>
-          <form onSubmit={handleSubmit}>
-            <label>
-              Add new bird:&nbsp;
-              <input type='text' value={query} onChange={(e) => setquery(e.target.value)} />
-            </label>
-            <input type='submit' value='Submit' />
-          </form>
-          {cards.map((data) => (
-            <Card
-              heading={data.heading}
-              description={data.description}
-              image={data.image}
-              id={data.id}
-              key={data.id}
-            />
-          ))}
+          <div className='controls'>
+            <form className='flex w-100pc' onSubmit={handleSubmit}>
+              <input
+                type='text'
+                placeholder='Add new card'
+                value={query}
+                onChange={(e) => setquery(e.target.value)}
+                autoFocus
+              />
+              <input type='submit' value='Submit' />
+              <button className='flex-item-right' onClick={() => setcards([])}>
+                Clear
+              </button>
+            </form>
+          </div>
+          {cards.length > 0 && (
+            <div className='cards-grid'>
+              {cards.map((data, idx) => (
+                <Card {...data} deleteCard={deleteCard} key={idx} />
+              ))}
+            </div>
+          )}
         </div>
       </main>
       <footer>&copy;KM 2020</footer>
--- src/Card.js
+++ src/Card.js
@@ -1,13 +1,45 @@
 import React from 'react'
 
+function HeaderRow(props) {
+  const { id, deleteCard, heading } = props
+  return (
+    <div className='flex'>
+      <h1 onClick={() => window.open(`http://en.wikipedia.org/?curid=${id}`)}>
+        {heading || 'Failed to get card heading :('}
+      </h1>
+      <span className='close flex-item-right' onClick={() => deleteCard(id)}>
+        &times;
+      </span>
+    </div>
+  )
+}
+
+const Thumbnail = (props) => {
+  const { image, heading } = props
+  return image ? <img src={image} alt={heading}></img> : <></>
+}
+
+const Description = (props) => {
+  const description = props.description
+  return description ? (
+    <div dangerouslySetInnerHTML={{ __html: description }} />
+  ) : (
+    <div>Failed to get description :(</div>
+  )
+}
+
 export default function Card(props) {
-  const { heading, description, image, id } = props
+  const { heading, description, image, id, deleteCard } = props
+  const headerProps = { id, deleteCard, heading }
+  const thumbnailProps = { image, heading }
 
   return (
-    <div className='card' onClick={() => window.open(`http://en.wikipedia.org/?curid=${id}`)}>
-      <h1>{heading || 'Card Heading'}</h1>
-      <img src={image} style={{ height: 'auto', maxHeight: '150px', width: 'auto' }} alt=''></img>
-      <div dangerouslySetInnerHTML={{ __html: description }} />
+    <div className='card entry-card'>
+      <div className='card-content ht-100pc'>
+        <HeaderRow {...headerProps} />
+        <Thumbnail {...thumbnailProps} />
+        <Description description={description} />
+      </div>
     </div>
   )
 }
